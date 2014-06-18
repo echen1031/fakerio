4f8646b3da31421774f9679835aa52ab9e8d16bc
Eric Chen <eric87chen@gmail.com>
2014-06-18 14:01:27 -0400

    Added Rack middleware for rate limiting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   
   protect_from_forgery with: :exception
-  before_filter :throttle
+  #before_filter :throttle
   
   def throttle
     client_ip = request.env["REMOTE_ADDR"]
